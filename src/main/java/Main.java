34448401bc4626b668798873c7039f1692776de6
Miguel Maza <anmac_tec@outlook.com>
2024-04-11 15:43:53 -0500

    pass 3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -14,7 +16,20 @@ public class Main {
       clientSocket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
 
-      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+      InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+      BufferedReader br = new BufferedReader(isr);
+      String[] parts = br.readLine().split(" ");
+
+      if (parts[1].equals("/")) {
+        clientSocket
+            .getOutputStream()
+            .write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+      } else {
+        clientSocket
+            .getOutputStream()
+            .write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+      }
+
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
