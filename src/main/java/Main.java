4271c1fddc3c29f3ed84060dd6487f531da1a673
Miguel Maza <anmac_tec@outlook.com>
2024-04-11 19:56:12 -0500

    pass 6th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,60 +7,67 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-
     try {
-      serverSocket = new ServerSocket(4221);
+      ServerSocket serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
 
-      InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-      BufferedReader br = new BufferedReader(isr);
-      String[] path = br.readLine().split(" ");
+      while (true) {
+        final Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+        Thread thread =
+            new Thread(
+                () -> {
+                  try {
+                    InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+                    BufferedReader br = new BufferedReader(isr);
+                    String[] path = br.readLine().split(" ");
 
-      if (path[1].equals("/")) {
-        clientSocket
-            .getOutputStream()
-            .write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
-      } else if (path[1].startsWith("/echo/")) {
-        String result = path[1].replaceFirst("^/echo/", "");
-        clientSocket
-            .getOutputStream()
-            .write(
-                ("HTTP/1.1 200 OK\r\n"
-                        + "Content-Type: text/plain\r\n"
-                        + "Content-Length: "
-                        + result.length()
-                        + "\r\n\r\n"
-                        + result
-                        + "\r\n\r\n")
-                    .getBytes(StandardCharsets.UTF_8));
-      } else if (path[1].startsWith("/user-agent")) {
-        String line;
-        while (!(line = br.readLine()).isEmpty()) {
-          if (line.toLowerCase().contains("user-agent")) {
-            String content = line.replaceFirst("User-Agent: ", "");
-            clientSocket
-                .getOutputStream()
-                .write(
-                    ("HTTP/1.1 200 OK\r\n"
-                            + "Content-Type: text/plain\r\n"
-                            + "Content-Length: "
-                            + content.length()
-                            + "\r\n\r\n"
-                            + content
-                            + "\r\n\r\n")
-                        .getBytes(StandardCharsets.UTF_8));
-          }
-        }
-      } else {
-        clientSocket
-            .getOutputStream()
-            .write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+                    if (path[1].equals("/")) {
+                      clientSocket
+                          .getOutputStream()
+                          .write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+                    } else if (path[1].startsWith("/echo/")) {
+                      String result = path[1].replaceFirst("^/echo/", "");
+                      clientSocket
+                          .getOutputStream()
+                          .write(
+                              ("HTTP/1.1 200 OK\r\n"
+                                      + "Content-Type: text/plain\r\n"
+                                      + "Content-Length: "
+                                      + result.length()
+                                      + "\r\n\r\n"
+                                      + result
+                                      + "\r\n\r\n")
+                                  .getBytes(StandardCharsets.UTF_8));
+                    } else if (path[1].startsWith("/user-agent")) {
+                      String line;
+                      while (!(line = br.readLine()).isEmpty()) {
+                        if (line.toLowerCase().contains("user-agent")) {
+                          String content = line.replaceFirst("User-Agent: ", "");
+                          clientSocket
+                              .getOutputStream()
+                              .write(
+                                  ("HTTP/1.1 200 OK\r\n"
+                                          + "Content-Type: text/plain\r\n"
+                                          + "Content-Length: "
+                                          + content.length()
+                                          + "\r\n\r\n"
+                                          + content
+                                          + "\r\n\r\n")
+                                      .getBytes(StandardCharsets.UTF_8));
+                        }
+                      }
+                    } else {
+                      clientSocket
+                          .getOutputStream()
+                          .write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+                    }
+                    System.out.println("accepted new connection");
+                  } catch (IOException e) {
+                    System.out.println("IOException: " + e.getMessage());
+                  }
+                });
+        thread.start();
       }
-
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
